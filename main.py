16b9f9cab6730f53055d954e738ee475e291981f
jevtovicc <jevtovic99@yahoo.com>
2025-07-28 19:58:53 +0200

    Fetch additional data about flags

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from pathlib import Path
 def extract_essential_data(data: list[dict]) -> list[dict]:
     return [{'country_name': entry['name']['common'], 
              'country_official_name': entry['name']['official'],
-             'flag': entry['flag'], 
+             'flag_png': entry['flags']['png'],
+             'flag_desc': entry['flags']['alt'], 
              'population': entry['population']
             } for entry in data]
 
@@ -21,7 +22,7 @@ def write_raw_data_to_json_file(data: list[dict], filepath: Path):
 if __name__ == '__main__':
     DATA_DIR = Path.cwd() / 'data'
     RAW_DIR = DATA_DIR / 'raw'
-    URL = 'https://restcountries.com/v3.1/all?fields=name,flag,population'
+    URL = 'https://restcountries.com/v3.1/all?fields=name,flags,population'
 
     response = requests.get(URL)
     data = response.json()
